2927408d889f30df73b87a52bd34b6c9b18f595c
Natalie Baker <baker-natalie@users.noreply.github.com>
2016-10-11 10:06:13 -0500

    style: update page styles

--- this-might-be-a-portfolio/js/helper.js
+++ this-might-be-a-portfolio/js/helper.js
@@ -1,28 +1,20 @@
 //the octopus
-var HTMLnavListItem = '<li class="nav-list-item"><a href="#%link%">%title%</a></li>';
+var HTMLnavListItem = '<li class="nav-list-item col-xs-3"><a href="#%link%">%title%</a></li>';
 var HTMLpageView = '<div class="anchor-div" id="%title%"><a class="anchor" name="%anchor%"></a></div>';
 var HTMLpageHeader = '<h1>%header%</h1>';
-var HTMLdivText = '<p class="content" id="%id%">%content%</p>';
-var HTMLbioImage = '<img src="%img%" alt="%alt%" />';
-var HTMLshowcaseDiv = '<div class="showcase"><h3>%title%</h3></div>';
-var HTMLimg = '<img src="%img%" alt="%alt%" />';
-var HTMLshowcaseLinks = '<a href="%code%">code</a><a href="%demo%">demo</a>';
-var HTMLprojects = '<div class="projects"><h3><a href="%link%">%title%</a></h3></div>';
-var HTMLservices = '<div class="services"></div>';
-var HTMLservice = '<div class="service"><h3>%title%</h3><p>%description%</p></div>';
+var HTMLdivText = '<p class="content col-xs-12" id="%id%">%content%</p>';
+var HTMLbioImage = '<img class="img-fluid col-xs-12" src="%img%" alt="%alt%" />';
+var HTMLshowcase = '<div id="showcase"></div>';
+var HTMLshowcaseDiv = '<div class="showcase"><h4>%title%</h4></div>';
+var HTMLimg = '<img class="img-fluid" src="%img%" alt="%alt%" />';
+var HTMLshowcaseLinks = '<br /><span><a href="%code%">code</a></span><span><a href="%demo%">demo</a></span>';
+var HTMLprojectDiv = '<div id="projects"></div>';
+var HTMLprojects = '<div class="projects"><h4><a href="%link%">%title%</a></h4></div>';
+var HTMLservices = '<div class="col-xs-1"></div><div class="services col-xs-10"></div><div class="col-xs-1"></div>';
+var HTMLservice = '<div class="service"><h4>%title%</h4><p class="service-detail">%description%</p></div>';
 var HTMLemailForm = '<div class="email-form"></div>';
-var HTMLsocMed = '<ul id="soc-med-list"></ul>';
+var HTMLsocMed = '<div id="soc-med-box"><ul id="soc-med-list"></ul></div>';
 var HTMLsocMedList = '</li><a href="%link%" class="soc-med-icon"></a></li>';
 var HTMLicon = '<img src="%img%" alt="%alt%"/>';
 
-var hideLanding = function(id) {
-	$(id).click(function() {
-		$( ".landing" ).slideToggle( "slow", function() {
-			$( ".landing" ).hide();
-			$(".soc-med-landing").hide();
-		});
-	});
-};
-hideLanding(".a");
-hideLanding(".b");
-hideLanding(".landing");
+
--- this-might-be-a-portfolio/js/main.js
+++ this-might-be-a-portfolio/js/main.js
@@ -3,66 +3,45 @@
 	var anchorLinks = {
 		list: [
 			{
-				header: "",
-				title: "home",
-				anchor: "home"
-			}, {
 				header: "About Me",
-				title: "about",
-				anchor: "about"
+				title: "about"
 			}, {
 				header: "Recent Work",
-				title: "portfolio",
-				anchor: "portfolio"
+				title: "portfolio"
 			}, {
 				header: "Let's Work Together",
-				title: "services",
-				anchor: "services"
+				title: "services"
 			}, {
 				header: "Talk to Me",
-				title: "contact",
-				anchor: "contact"
+				title: "contact"
 			}
 		]
 	};
 	anchorLinks.display = function() {
 		for (var i = 0; i < anchorLinks.list.length; i++) {
 			var formattedNavListItem = HTMLnavListItem.replace("%title%", anchorLinks.list[i].title);
-			$(".nav-list:last").append(formattedNavListItem.replace("%link%", anchorLinks.list[i].anchor));
-			var formattedPageView = HTMLpageView.replace("%title%", anchorLinks.list[i].title);
-			$(".anchor-container").append(formattedPageView.replace("%anchor%", anchorLinks.list[i].title));
+			$(".nav-list:last").append(formattedNavListItem.replace("%link%", anchorLinks.list[i].title));
+			var formattedPageView = HTMLpageView.replace("%anchor%", anchorLinks.list[i].title);
+			$(".anchor-container").append(formattedPageView.replace("%title%", anchorLinks.list[i].title));
 			$(".anchor-div:last").append(HTMLpageHeader.replace("%header%", anchorLinks.list[i].header));
 
 		};
 	};
 	anchorLinks.display();
 
-	var home = {
-		content: "design.<br />develop.<br />deploy.",
-		id: "home-text"
-	};
-	home.display = function() {
-		var formattedDivText = HTMLdivText.replace("%content%", home.content);
-		$("#home").append(formattedDivText.replace("%id%", home.id));
-	};
-	home.display();
-
 	var about = {
 		id: "about-me",
-		content: "My name is Natalie Baker, and I design, develop, and deploy beautiful, functional websites using HTML, CSS, and JavaScript. I have experience with HTML5 Canvas, Bootstrap, Knockout.js, APIs, Jasmine testing, and responsive design. I am dedicated to building optimized, accessible websites with strategically designed user interfaces.<br /><br />I am currently learning React.js and Sass to complete Free Code Camp's Data Visualization projects. I am also interested in learning more Javascript frameworks and back-end development. In addition to continuing my education, I am also working and gaining experience as a freelance developer. I am currently seeking new clients, as well as full-time opportunities.",
-		img: "",
-		alt: ""
+		content: "My name is Natalie Baker, and I design, develop, and deploy beautiful, functional websites using HTML, CSS, and JavaScript. I have experience with HTML5 Canvas, Bootstrap, Knockout.js, APIs, Jasmine testing, and responsive design. I am dedicated to building optimized, accessible websites with strategically designed user interfaces.<br /><br />I am currently learning React.js and Sass to complete Free Code Camp's Data Visualization projects. I am also interested in learning more Javascript frameworks and back-end development. In addition to continuing my education, I am also working and gaining experience as a freelance developer. I am currently seeking new clients, as well as full-time opportunities."
 	};
 	about.display = function() {
 		var formattedDivText = HTMLdivText.replace("%content%", about.content);
 		$("#about").append(formattedDivText.replace("%id%", about.id));
-		var formattedBioImage = HTMLbioImage.replace("%img%", about.img);
-		$("#about").append(formattedBioImage.replace("%alt%", about.alt));
+		$("#about-me").addClass("col-md-8");
 	};
 	about.display();
 
 	var recentWork = {
-		intro: 'See all my work at <a href="https://github.com/baker-natalie">github</a> and <a href="http://codepen.io/nataliebaker">codepen</a>.',
+		intro: '<p class="project-p">See all my work at <a href="https://github.com/baker-natalie">github</a> and <a href="http://codepen.io/nataliebaker">codepen</a>.</p>',
 		showcase: [
 			{
 				title: "Dillard Farms",
@@ -85,7 +64,7 @@
 				img: "images/web-perf.jpg",
 				alt: "hello sweetie thumbnail"
 			}, {
-				title: "Jasmine Feed Reader Testing",
+				title: "Feed Reader Testing",
 				url: "https://github.com/baker-natalie/feed-reader-testing",
 				img: "images/jasmine-testing.jpg",
 				alt: "feed reader testing thumbnail"
@@ -104,8 +83,11 @@
 	};
 	recentWork.display = function() {
 		$("#portfolio").append(recentWork.intro);
+		$("#portfolio").append(HTMLshowcase);
+		$("#portfolio").append(HTMLprojectDiv);
+
 		for (var i = 0; i < recentWork.showcase.length; i++) {
-			$("#portfolio").append(HTMLshowcaseDiv.replace("%title%", recentWork.showcase[i].title));
+			$("#showcase").append(HTMLshowcaseDiv.replace("%title%", recentWork.showcase[i].title));
 			var formattedShowcaseImg = HTMLimg.replace("%img%", recentWork.showcase[i].img);
 			$(".showcase:last").append(formattedShowcaseImg.replace("%alt%", recentWork.showcase[i].alt));
 			var formattedShowcaseLinks = HTMLshowcaseLinks.replace("%code%", recentWork.showcase[i].code);
@@ -113,7 +95,7 @@
 		};
 		for (var j = 0; j < recentWork.projects.length; j++) {
 			var formattedProjects = HTMLprojects.replace("%title%", recentWork.projects[j].title);
-			$("#portfolio").append(formattedProjects.replace("%link%", recentWork.projects[j].url));
+			$("#projects").append(formattedProjects.replace("%link%", recentWork.projects[j].url));
 			var formattedProjectImg = HTMLimg.replace("%img%", recentWork.projects[j].img);
 			$(".projects:last").append(formattedProjectImg.replace("%alt%", recentWork.projects[j].alt));
 		};
@@ -124,24 +106,27 @@
 		serviceItems: [
 			{
 				title: "Full Build",
-				description: "Look, I can take you as far as Anchorhead. You can get a transport there to Mos Eisley or wherever you're going. Escape is not his plan. I must face him, alone. What?! I want to come with you to Alderaan. There's nothing for me here now. I want to learn the ways of the Force and be a Jedi, like my father before me."
+				description: "Starting from scratch? We'll work together to build the website you need, and reach the audience you are trying to impact."
 			}, {
 				title: "Project Assistance",
-				description: "I'm trying not to, kid. I suggest you try it again, Luke. This time, let go your conscious self and act on instinct. Don't act so surprised, Your Highness. You weren't on any mercy mission this time. Several transmissions were beamed to this ship by Rebel spies. I want to know what happened to the plans they sent you."
+				description: "Does your existing website need an update? I'll help you with everything from minor bug fixes to complete redesign. "
 			}, {
 				title: "Custom Project",
-				description: "What?! You don't believe in the Force, do you? Remember, a Jedi can feel the Force flowing through him. What good is a reward if you ain\'t around to use it? Besides, attacking that battle station ain\'t my idea of courage. It\'s more like…suicide."
+				description: "Got an idea for a web-based app? Let me help you bring it to life."
 			}, {
 				title: "Full Time",
-				description: "I care. So, what do you think of her, Han? Red Five standing by. You mean it controls your actions?"
+				description: "Need a crackerjack junior developer to join your team? Let's talk!"
 			}
-		]
+		],
+		contactLink: '<h2 class="col-xs-12"><a href="#contact">Get in Touch!</a></h2>'
 	};
 	services.display = function() {
+		$("#services").append(HTMLservices);
 		for (var i = 0; i < services.serviceItems.length; i++) {
 			var formattedService = HTMLservice.replace("%title%", services.serviceItems[i].title);
-			$("#services").append(formattedService.replace("%description%", services.serviceItems[i].description));
+			$(".services").append(formattedService.replace("%description%", services.serviceItems[i].description));
 		};
+		$("#services").append(services.contactLink);
 	};
 	services.display();
 
@@ -172,11 +157,6 @@
 			$("#soc-med-list").append(HTMLsocMedList.replace('%link%', socialMedia.socMedItems[i].link));
 			var formattedIcon = HTMLicon.replace('%img%', socialMedia.socMedItems[i].img);
 			$(".soc-med-icon:last").append(formattedIcon.replace('%alt%', socialMedia.socMedItems[i].alt));
-		}
-		for (var j = 0; j < socialMedia.socMedItems.length; j++) {
-			$(".soc-med-landing").append(HTMLsocMedList.replace('%link%', socialMedia.socMedItems[j].link));
-			var formattedIcon = HTMLicon.replace('%img%', socialMedia.socMedItems[j].img);
-			$(".soc-med-icon:last").append(formattedIcon.replace('%alt%', socialMedia.socMedItems[j].alt));
 		}
 	};
 	socialMedia.display();
